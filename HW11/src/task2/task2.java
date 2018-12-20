475d2ee229751e809290d04aa3f4773742ff77bc
rusl-krow <43133917+rusl-krow@users.noreply.github.com>
2018-12-21 02:56:17 +0300

    domashka11

---
+++ HW11/src/task2/task2.java
@@ -0,0 +1,58 @@
+package task2;
+
+
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+
+import java.io.PrintWriter;
+
+import java.util.Scanner;
+
+
+
+//  №2 В двух файлах хранятся отсортированные числа, каждое на новой строчке.
+//  Реализовать операцию слияния в один тоже отсортированный файл. Массивы
+//  использовать нельзя. По каждому файлу можно пройтись лишь один раз.
+
+
+public class task2 {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file1 = new File("src\\task2\\in1.txt");
+        File file2 = new File("src\\task2\\in2.txt");
+
+
+        Scanner sc1 = new Scanner(file1);
+        Scanner sc2 = new Scanner(file2);
+
+        PrintWriter pw = new PrintWriter("src\\task2\\out.txt");
+
+        int a = sc1.nextInt();
+        int b = sc2.nextInt();
+
+
+        while (sc1.hasNextInt() || sc2.hasNextInt()) {
+            if (a < b) {
+                pw.println(a);
+                if (sc1.hasNextInt()) a = sc1.nextInt();
+                else a = sc2.nextInt();
+            } else {
+                pw.println(b);
+                if (sc2.hasNextInt()) b = sc2.nextInt();
+                else b = sc1.nextInt();
+            }
+        }
+        if (a < b) {
+            pw.println(a);
+            pw.println(b);
+        } else {
+            pw.println(b);
+            pw.println(a);
+        }
+        pw.close();
+    }
+}
