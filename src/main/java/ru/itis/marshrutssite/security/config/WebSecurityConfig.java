62b962ba4b4f42c8ef5edb9169a49e0cb2f5eff5
rusl-krow <43133917+rusl-krow@users.noreply.github.com>
2020-05-21 00:12:32 +0300

    semestrovka

---
+++ src/main/java/ru/itis/marshrutssite/security/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package ru.itis.marshrutssite.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.itis.marshrutssite.models.Role;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier(value = "customUserDetailsService")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/registration","/confirm/**","/signIn","/").permitAll()
+                .antMatchers("/main").authenticated()
+                .antMatchers("/admin").hasRole("ADMIN");
+
+
+        http.formLogin()
+                .loginPage("/signIn")
+                .usernameParameter("email")
+                .defaultSuccessUrl("/")
+                .failureUrl("/signIn?error")
+                .permitAll();
+    }
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+}
